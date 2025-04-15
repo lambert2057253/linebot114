b9d31ff1b811ee6347ab7e2214f3664b3ea4b11a
lambert2057253 <100283118+lambert2057253@users.noreply.github.com>
2025-04-15 14:00:02 +0800

    Update Msg_News.py

--- msg_template/Msg_News.py
+++ msg_template/Msg_News.py
@@ -70,7 +70,8 @@ def single_stock(stockNumber):
             "type": "bubble",
             "hero": {
                 "type": "image",
-                "url": "https://i.imgur.com/uvrIuT9.jpg",
+                "url": "https://img.lovepik.com/free-png/20211126/lovepik-computer-watching-news-png-image_401167758_wh1200.png",
+                #"url": "https://i.imgur.com/uvrIuT9.jpg",
                 "size": "full",
                 "aspectRatio": "20:13",
                 "aspectMode": "fit",
